e84dfc154e80695fd8b234d34dc2ce6231cf77f7
Leonardo Gutierrez <tilarian@outlook.com>
2023-06-21 11:15:50 -0300

    Se cambia calculadora por ecommerce

--- index.js
+++ index.js
@@ -1,37 +1,92 @@
-// Funciones flecha
-const sumar = (num1, num2) => num1 + num2;
-const restar = (num1, num2) => num1 - num2;
-const multiplicar = (num1, num2) => num1 * num2;
-const dividir = (num1, num2) => num1 / num2;
+const OPCION_SALIR = 4;
+const IVA = 1.22;
+let TotalDeProducto = 1000;
+const ArrayDeProductos = []
 
-// Se solicita input numérico al usuario
-const num1 = parseInt(prompt("Ingrese el primer número:"));
-const num2 = parseInt(prompt("Ingrese el segundo número:"));
-
-// Se muestran las operaciones disponibles en consola
-console.log("Operaciones disponibles: ");
-console.log("- Suma (+)");
-console.log("- Resta (-)");
-console.log("- Multiplicación (*)");
-console.log("- División (/)");
-
-// Se solicita input de la operación
-const operacion = prompt("Ingrese la operación que desea realizar");
-
-// Se llama la función correspondiente según la operación ingresada
-switch (operacion) {
-  case "+":
-    console.log(`El resultado de la suma es: ${sumar(num1, num2)}`);
-    break;
-  case "-":
-    console.log(`El resultado de la resta es: ${restar(num1, num2)}`);
-    break;
-  case "*":
-    console.log(`El resultado de la multiplicación es: ${multiplicar(num1, num2)}`);
-    break;
-  case "/":
-    console.log(`El resultado de la división es: ${dividir(num1, num2)}`);
-    break;
-  default:
-    console.log("Operación inválida. Por favor, inténtelo de nuevo.");
+class Producto {
+        constructor(nombre, precio, categoria) {
+                this.nombre = nombre;
+                this.precio = precio;
+                this.categoria = categoria;
+        }
 }
+
+const celular1 = new Producto("Samsung Galaxy", 1000, "celulares");
+ArrayDeProductos.push(celular1);
+
+const celular2 = new Producto("Iphone", 1000, "celulares");
+ArrayDeProductos.push(celular2);
+
+const auricular1 = new Producto("Auriculares Hyperx", 1000, "perifericos");
+ArrayDeProductos.push(auricular1);
+
+const auricular2 = new Producto("Auriculares Logitech", 1000, "perifericos");
+ArrayDeProductos.push(auricular2);
+
+const consola1 = new Producto("Playstation 5", 1000, "consolas");
+ArrayDeProductos.push(consola1);
+
+const consola2 = new Producto("Playstation 4", 1000, "consolas");
+ArrayDeProductos.push(consola2);
+
+
+
+const mostrarProductoPorCategoria = (categoria) => {
+        const filtrado = ArrayDeProductos.filter((el) => el.categoria === categoria);
+        let mensajeAMostrar = '';
+        filtrado.forEach((el) => {
+                mensajeAMostrar += `Producto: ${el.nombre} Precio: ${el.precio}`
+        })
+        alert(mensajeAMostrar);
+}
+
+
+
+const verProductos = () => {
+        let opcion;
+        opcion = parseInt(prompt("elige la operacion que deseas, \n 1-Celulares \n 2-Perifericos \n 3-Consolas \n 4-Volver Atras "));
+        while (opcion != OPCION_SALIR) {
+                switch (opcion) {
+                        case 1:
+                                mostrarProductoPorCategoria("celulares");
+                                break;
+                        case 2: mostrarProductoPorCategoria("perifericos");
+                                break;
+                        case 3: mostrarProductoPorCategoria("consolas");
+                                break;
+                        default:
+                                alert("Ingreso una opcion invalida.");
+                                break;
+                }
+                opcion = parseInt(prompt("elige la operacion que deseas, \n 1-Celulares \n 2-Perifericos \n 3-Consolas \n 4-Volver Atras "))
+        }
+}
+
+const calcularIVA = (total) => {
+        const resultado = IVA * total
+        alert("El total con IVA: $" + resultado + "\n El total es: $" + total);
+}
+
+let opcion = parseInt(prompt("elige la operacion que deseas, \n 1-Ver Productos \n 2-Ver Operacion \n 3-Calcular IVA \n 4 Salir "))
+
+while (opcion != OPCION_SALIR) {
+
+        switch (opcion) {
+                case 1:
+                        verProductos();
+                        break;
+                case 2:
+                        verOperacion();
+                        break;
+                case 3:
+                        calcularIVA(TotalDeProducto);
+                        break;
+                default:
+                        alert("Ingreso una opcion invalida.");
+                        break;
+        }
+
+        opcion = parseInt(prompt("elige la operacion que deseas, \n 1-Ver Productos \n 2-Ver Operacion \n 3-Calcular IVA \n 4 Salir "));
+}
+
+alert("Gracias vuelva pronto.")
